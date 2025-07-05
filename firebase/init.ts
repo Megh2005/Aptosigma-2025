ce33943465547b5e4b389aee520581615ed50222
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2025-07-05 19:12:33 +0530

    fix(firebase): update Firebase configuration to use hardcoded values and initialize authentication

--- firebase/init.ts
+++ firebase/init.ts
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCWmzPYgzOAB65jz7krjep0V5n-dPwsSjs",
+  authDomain: "apto-sigma.firebaseapp.com",
+  projectId: "apto-sigma",
+  storageBucket: "apto-sigma.firebasestorage.app",
+  messagingSenderId: "306488627106",
+  appId: "1:306488627106:web:772cf4a6dd679450bd6cfb",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
